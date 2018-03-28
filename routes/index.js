9b176134888f9d6fa99f04aee507e54cb45b6309
linzhiyong <linzhiyong@heshidai.com>
2018-03-28 18:29:03 +0800

    新增模板

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
+
+module.exports = router;
